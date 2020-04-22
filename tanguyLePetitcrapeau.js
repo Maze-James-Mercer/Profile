0ad6dec619fdeb8a6028a741ae76f26f6ab147f7
Maze-James-Mercer <63016688+Maze-James-Mercer@users.noreply.github.com>
2020-04-22 05:31:32 +0200

    Add files via upload

---
+++ tanguyLePetitcrapeau.js
@@ -0,0 +1,184 @@
+
+
+function main() {
+    
+    //OPEN / CLOSE BANNERS
+
+    // Section - Education
+
+    document.getElementById("btn-open-edhec").addEventListener("click", function() {
+        document.getElementById("banner-edhec").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-edhec").addEventListener("click", function() {
+        document.getElementById("banner-edhec").style.display = "none";
+    });
+    
+    // var edhec = document.getElementById("banner-edhec").style.display
+    // if (edhec == "flex") {
+    //     document.getElementsByClassName("container-2").style.opacity = 0;
+    // }
+
+
+    document.getElementById("btn-open-exam").addEventListener("click", function() {
+        document.getElementById("banner-exam").style.display = "flex";
+    });
+
+    document.getElementById("btn-close-exam").addEventListener("click", function() {
+        document.getElementById("banner-exam").style.display = "none";
+    });
+
+    document.getElementById("btn-open-recamier").addEventListener("click", function() { 
+        document.getElementById("banner-recamier").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-recamier").addEventListener("click", function() {
+        document.getElementById("banner-recamier").style.display = "none";
+    });
+
+    document.getElementById("btn-open-lyon2").addEventListener("click", function() { 
+        document.getElementById("banner-lyon2").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-lyon2").addEventListener("click", function() {
+        document.getElementById("banner-lyon2").style.display = "none";
+    });
+
+    document.getElementById("btn-open-blaise").addEventListener("click", function() { 
+        document.getElementById("banner-blaise").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-blaise").addEventListener("click", function() {
+        document.getElementById("banner-blaise").style.display = "none";
+    });
+
+
+    // Section - Experience
+
+    document.getElementById("btn-open-homerun").addEventListener("click", function() {
+        document.getElementById("banner-homerun").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-homerun").addEventListener("click", function() {
+        document.getElementById("banner-homerun").style.display = "none";
+    });
+    
+    document.getElementById("btn-open-tee").addEventListener("click", function() {
+        document.getElementById("banner-tee").style.display = "flex";
+    });
+
+    document.getElementById("btn-close-tee").addEventListener("click", function() {
+        document.getElementById("banner-tee").style.display = "none";
+    });
+
+    document.getElementById("btn-open-soap").addEventListener("click", function() { 
+        document.getElementById("banner-soap").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-soap").addEventListener("click", function() {
+        document.getElementById("banner-soap").style.display = "none";
+    });
+
+    document.getElementById("btn-open-tutor").addEventListener("click", function() { 
+        document.getElementById("banner-tutor").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-tutor").addEventListener("click", function() {
+        document.getElementById("banner-tutor").style.display = "none";
+    });
+
+    document.getElementById("btn-open-fiducial").addEventListener("click", function() { 
+        document.getElementById("banner-fiducial").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-fiducial").addEventListener("click", function() {
+        document.getElementById("banner-fiducial").style.display = "none";
+    });
+
+
+    // Section - Skills
+
+    document.getElementById("btn-open-spirit").addEventListener("click", function() {
+        document.getElementById("banner-spirit").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-spirit").addEventListener("click", function() {
+        document.getElementById("banner-spirit").style.display = "none";
+    });
+    
+    document.getElementById("btn-open-management").addEventListener("click", function() {
+        document.getElementById("banner-management").style.display = "flex";
+    });
+
+    document.getElementById("btn-close-management").addEventListener("click", function() {
+        document.getElementById("banner-management").style.display = "none";
+    });
+
+    document.getElementById("btn-open-languages").addEventListener("click", function() { 
+        document.getElementById("banner-languages").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-languages").addEventListener("click", function() {
+        document.getElementById("banner-languages").style.display = "none";
+    });
+
+    document.getElementById("btn-open-code").addEventListener("click", function() { 
+        document.getElementById("banner-code").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-code").addEventListener("click", function() {
+        document.getElementById("banner-code").style.display = "none";
+    });
+
+    document.getElementById("btn-open-software").addEventListener("click", function() { 
+        document.getElementById("banner-software").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-software").addEventListener("click", function() {
+        document.getElementById("banner-software").style.display = "none";
+    });
+
+
+    // Section - Hobbies
+
+    document.getElementById("btn-open-music").addEventListener("click", function() {
+        document.getElementById("banner-music").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-music").addEventListener("click", function() {
+        document.getElementById("banner-music").style.display = "none";
+    });
+    
+    document.getElementById("btn-open-development").addEventListener("click", function() {
+        document.getElementById("banner-development").style.display = "flex";
+    });
+
+    document.getElementById("btn-close-development").addEventListener("click", function() {
+        document.getElementById("banner-development").style.display = "none";
+    });
+
+    document.getElementById("btn-open-gym").addEventListener("click", function() { 
+        document.getElementById("banner-gym").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-gym").addEventListener("click", function() {
+        document.getElementById("banner-gym").style.display = "none";
+    });
+
+    document.getElementById("btn-open-travel").addEventListener("click", function() { 
+        document.getElementById("banner-travel").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-travel").addEventListener("click", function() {
+        document.getElementById("banner-travel").style.display = "none";
+    });
+
+    document.getElementById("btn-open-drawing").addEventListener("click", function() { 
+        document.getElementById("banner-drawing").style.display = "flex";
+    });
+    
+    document.getElementById("btn-close-drawing").addEventListener("click", function() {
+        document.getElementById("banner-drawing").style.display = "none";
+    });
+
+}
